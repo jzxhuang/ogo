db2659f1cfd255d9559c102067dc67f22e2f7615
Jeff Huang <jeff@spatial.io>
2023-01-28 23:58:39 -0500

    init browser extension app w/ plasmo

---
+++ apps/browser-extension/src/popup.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+function IndexPopup() {
+  const [data, setData] = useState('')
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 16,
+      }}
+    >
+      <h2>
+        Welcome to Ogo{' '}
+        <a href="https://www.plasmo.com" target="_blank" rel="noreferrer">
+          Plasmo
+        </a>{' '}
+        Extension!
+      </h2>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <a href="https://docs.plasmo.com" target="_blank" rel="noreferrer">
+        View Docs
+      </a>
+    </div>
+  )
+}
+
+export default IndexPopup
